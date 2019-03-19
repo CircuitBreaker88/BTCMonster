7e7eabb7891efb32ff1881a3e3eb7522b0d597dd
CircuitBreaker88 <sarris88p@gmail.com>
2019-03-19 19:59:43 -0400

    Test Implementation

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -210,7 +210,7 @@ void CActiveMasternode::ManageStateInitial()
         return;
     }
 
-    if(pwalletMain->GetBalance() < 1000*COIN) {
+    if(pwalletMain->GetBalance() < IsMasternodeCollateral(vout[i].nValue)) {
         LogPrintf("CActiveMasternode::ManageStateInitial -- %s: Wallet balance is < 1000 MON\n", GetStateString());
         return;
     }
@@ -229,7 +229,7 @@ void CActiveMasternode::ManageStateInitial()
 
 void CActiveMasternode::ManageStateRemote()
 {
-    LogPrint("masternode", "CActiveMasternode::ManageStateRemote -- Start status = %s, type = %s, pinger enabled = %d, pubKeyMasternode.GetID() = %s\n", 
+    LogPrint("masternode", "CActiveMasternode::ManageStateRemote -- Start status = %s, type = %s, pinger enabled = %d, pubKeyMasternode.GetID() = %s\n",
              GetStatus(), fPingerEnabled, GetTypeString(), pubKeyMasternode.GetID().ToString());
 
     mnodeman.CheckMasternode(pubKeyMasternode);
