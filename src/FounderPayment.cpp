c66c85ff880443570914ecf5202e1b391392ea24
CircuitBreaker88 <sarris88p@gmail.com>
2019-03-26 13:25:15 -0400

    Fix Founder Block

--- src/FounderPayment.cpp
+++ src/FounderPayment.cpp
@@ -17,7 +17,7 @@
 #include <boost/foreach.hpp>
 
 CAmount FounderPayment::getFounderPaymentAmount(int blockHeight, CAmount blockReward) {
-	 if (blockHeight < 165000){
+	 if (blockHeight < 166250){
 		 return 0;
 	 }
 	 return blockReward * 0.05;
