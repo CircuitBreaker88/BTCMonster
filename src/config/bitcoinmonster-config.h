5b9f3c57d37ef44c5b54a174d787f7cfbe686451
CircuitBreaker88 <sarris88p@gmail.com>
2019-03-27 02:20:58 -0400

    Vesion Bump
    v2.0.1 Fix

--- src/config/bitcoinmonster-config.h
+++ src/config/bitcoinmonster-config.h
@@ -9,7 +9,7 @@
 /* #undef AC_APPLE_UNIVERSAL_BUILD */
 
 /* Version Build */
-#define CLIENT_VERSION_BUILD 0
+#define CLIENT_VERSION_BUILD 1
 
 /* Version is release */
 #define CLIENT_VERSION_IS_RELEASE true
@@ -308,7 +308,7 @@
 #define PACKAGE_NAME "BitcoinMonster Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "BitcoinMonster Core 0.12.1"
+#define PACKAGE_STRING "BitcoinMonster Core 2.0.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bitcoinmonstercore"
@@ -317,7 +317,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.12.1"
+#define PACKAGE_VERSION "2.0.1"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
